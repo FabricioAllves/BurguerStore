541621316e6386ce37734580daedc55bb662d12f
henrique26 <fabricioohh@gmail.com>
2024-07-30 23:52:37 -0300

    Initial Commit

---
+++ BurguerStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurguerStore
+//
+//  Created by Fabricio Henrique on 30/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
